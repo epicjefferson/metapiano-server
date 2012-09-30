01159d72e9c09810c11747143e34f2cb8ae25e1a
Guy John <guy@rumblesan.com>
2012-09-30 12:50:03 +0100

    Controller now no longer needs to start PD

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -10,14 +10,6 @@ object Application extends Controller {
   
   def index = Action {
 
-    val pdExe = current.configuration.getString("patchwerk.puredata").get
-    val port = current.configuration.getInt("patchwerk.port").get
-    val patch = current.configuration.getString("patchwerk.patch").get
-    val paths = current.configuration.getString("patchwerk.paths").get.split(",").toList
-    val extras = List.empty[String]
-
-    PureData.startPD(pdExe, port, patch, paths, extras)
-
     Ok("Yup it's running")
 
   }
