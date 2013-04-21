b5e6bd155d1e416f4d784e125b0ead98d96a655f
Guy John <guy@rumblesan.com>
2013-04-21 21:47:36 +0100

    PD Listener now writes to logfile

--- app/puredata/PureData.scala
+++ app/puredata/PureData.scala
@@ -1,6 +1,6 @@
 package com.rumblesan.patchwerk
 
-import com.rumblesan.scalapd.PureDataManager
+import com.rumblesan.scalapd.{ PureDataManager, FileLogger, LogMessage }
 import com.rumblesan.scalapd.network.PDMessage
 
 import play.api.libs.concurrent._
@@ -11,9 +11,33 @@ import play.api.Play.current
 
 object PureData {
 
-  lazy val manager = Akka.system.actorOf(Props[PureDataManager])
+  val logFileName = current.configuration.getString("patchwerk.logfile").get
+  val listener = Props(new PatchwerkListener(logFileName))
+  lazy val manager = Akka.system.actorOf(Props(new PureDataManager(listener)))
 
   lazy val patchwerk = Akka.system.actorOf(Props[Patchwerk])
+
+}
+
+
+class PatchwerkListener(logFileName: String) extends Actor {
+
+  import java.io.{BufferedWriter, FileWriter}
+
+  val fileOut = new BufferedWriter(new FileWriter(logFileName))
+
+  def writeToLog(output: String) {
+    fileOut.write(output + "\n")
+    fileOut.flush()
+  }
+
+  def receive = {
+
+    case LogMessage(line) => {
+      writeToLog(line)
+    }
+
+  }
 
 }
 
