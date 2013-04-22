2846fd625fc36aaa8b350b56012f8700142c827e
Guy John <guy@rumblesan.com>
2013-04-22 21:25:01 +0100

    Adding endpoint to send messages to PD

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,11 +4,27 @@ import play.api._
 import play.api.mvc._
 import play.api.Play.current
 
+import com.rumblesan.scalapd._
+import com.rumblesan.patchwerk.PureData
+
+import play.api.libs.json._
+
 object Application extends Controller {
   
   def index = Action {
 
-    Ok("Yup it's running")
+    Ok(views.html.main())
+
+  }
+
+  def sendmessage = Action(parse.json) { implicit request =>
+
+    request.body.validate[Map[String,String]].map( data => {
+      data.get("message").map(m => {
+        PureData.manager ! SendPDMessage(m.split(" ").toList)
+        Ok("")
+      }).getOrElse(BadRequest("No message in JSON"))
+    }).getOrElse(BadRequest("Incorrect Json"))
 
   }
   
