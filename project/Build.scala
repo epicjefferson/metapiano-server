4948891740614684667e521c8fb51914606e16ec
Guy John <guy@rumblesan.com>
2013-09-08 18:55:56 +0100

    Bumping version number

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import play.Project._
 object ApplicationBuild extends Build {
 
     val appName         = "metapiano"
-    val appVersion      = "1.0"
+    val appVersion      = "2.0"
 
     val appDependencies = Seq(
       jdbc,
